ebe63e360ff5ade6c973c7b796c54b0d45f75372
iancost4 <iancosta_k@hotmail.com>
2024-12-04 00:48:17 -0500

    fix: adicionando social media

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -86,14 +86,6 @@ function Sitemap() {
   )
 }
 
-function SocialIconX(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg viewBox="0 0 16 16" fill="currentColor" {...props}>
-      <path d="M12.6 0h2.454l-5.36 6.778L16 16h-4.937l-3.867-5.594L2.771 16H.316l5.733-7.25L0 0h5.063l3.495 5.114L12.6 0zm-.86 14.376h1.36L4.323 1.539H2.865l8.875 12.837z" />
-    </svg>
-  )
-}
-
 function SocialIconFacebook(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
     <svg viewBox="0 0 16 16" fill="currentColor" {...props}>
@@ -109,7 +101,29 @@ function SocialIconFacebook(props: React.ComponentPropsWithoutRef<'svg'>) {
 function SocialIconLinkedIn(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
     <svg viewBox="0 0 16 16" fill="currentColor" {...props}>
-      <path d="M14.82 0H1.18A1.169 1.169 0 000 1.154v13.694A1.168 1.168 0 001.18 16h13.64A1.17 1.17 0 0016 14.845V1.15A1.171 1.171 0 0014.82 0zM4.744 13.64H2.369V5.996h2.375v7.644zm-1.18-8.684a1.377 1.377 0 11.52-.106 1.377 1.377 0 01-.527.103l.007.003zm10.075 8.683h-2.375V9.921c0-.885-.015-2.025-1.234-2.025-1.218 0-1.425.966-1.425 1.968v3.775H6.233V5.997H8.51v1.05h.032c.317-.601 1.09-1.235 2.246-1.235 2.405-.005 2.851 1.578 2.851 3.63v4.197z" />
+      <path
+        d="M14.82 0H1.18A1.169 1.169 0 000 1.154v13.694A1.168 1.168 0 001.18 16h13.64A1.17 1.17 0 0016 14.845V1.15A1.171 1.171 0 0014.82 0zM4.744 13.64H2.369V5.996h2.375v7.644zm-1.18-8.684a1.377 1.377 0 11.52-.106 1.377 1.377 0 01-.527.103l.007.003zm10.075 8.683h-2.375V9.921c0-.885-.015-2.025-1.234-2.025-1.218 0-1.425.966-1.425 1.968v3.775H6.233V5.997H8.51v1.05h.032c.317-.601 1.09-1.235 2.246-1.235 2.405-.005 2.851 1.578 2.851 3.63v4.197z"
+      />
+    </svg>
+  )
+}
+
+function SocialIconWhatsApp(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
+      <path
+        d="M17.472 14.807c-.297-.148-1.758-.867-2.029-.967-.271-.1-.469-.148-.667.148-.198.297-.767.966-.94 1.164-.173.198-.347.222-.644.074-.297-.148-1.255-.462-2.39-1.475-.883-.786-1.48-1.754-1.654-2.051-.173-.297-.018-.458.13-.606.133-.132.297-.347.446-.52.148-.173.198-.297.297-.495.099-.198.05-.37-.025-.519-.074-.148-.667-1.611-.914-2.208-.241-.579-.486-.5-.667-.51h-.567c-.198 0-.519.074-.792.37-.272.297-1.04 1.015-1.04 2.479 0 1.463 1.064 2.877 1.213 3.074.148.198 2.094 3.2 5.07 4.487.709.306 1.26.489 1.69.626.71.227 1.36.195 1.871.118.571-.085 1.758-.719 2.007-1.412.247-.694.247-1.287.173-1.412-.074-.125-.271-.198-.567-.347z"
+      />
+    </svg>
+  )
+}
+
+function SocialIconInstagram(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
+      <path
+        d="M12 2.163c3.204 0 3.584.012 4.849.07 1.366.062 2.633.333 3.608 1.31.975.977 1.247 2.246 1.31 3.611.058 1.265.068 1.645.068 4.846s-.01 3.584-.068 4.849c-.063 1.366-.335 2.633-1.31 3.608-.975.975-2.244 1.247-3.608 1.31-1.265.058-1.645.068-4.849.068s-3.584-.01-4.849-.068c-1.366-.063-2.633-.335-3.608-1.31-.975-.975-1.247-2.244-1.31-3.608-.058-1.265-.068-1.645-.068-4.849s.01-3.584.068-4.849c.063-1.365.335-2.633 1.31-3.608.975-.975 2.244-1.247 3.608-1.31C8.416 2.175 8.796 2.163 12 2.163zm0-2.163C8.747 0 8.333.012 7.052.07 5.77.128 4.68.42 3.757 1.344c-.923.923-1.216 2.014-1.274 3.296C2.012 5.923 2 6.337 2 9.59c0 3.253.012 3.667.07 4.949.058 1.282.35 2.373 1.274 3.296.923.923 2.014 1.216 3.296 1.274 1.282.058 1.696.07 4.949.07s3.667-.012 4.949-.07c1.282-.058 2.373-.351 3.296-1.274.923-.923 1.216-2.014 1.274-3.296.058-1.282.07-1.696.07-4.949s-.012-3.667-.07-4.949c-.058-1.282-.351-2.373-1.274-3.296-.923-.923-2.014-1.216-3.296-1.274C15.667.012 15.253 0 12 0zM12 5.838a6.162 6.162 0 1 0 6.162 6.162A6.168 6.168 0 0 0 12 5.838zm0 10.17a4.007 4.007 0 1 1 4.007-4.007A4.013 4.013 0 0 1 12 16.008zm6.406-11.845a1.44 1.44 0 1 1-1.44-1.44 1.438 1.438 0 0 1 1.44 1.44z"
+      />
     </svg>
   )
 }
@@ -118,28 +132,28 @@ function SocialLinks() {
   return (
     <>
       <Link
-        href="https://facebook.com"
+        href="https://www.facebook.com/people/Animai-Festival/100067989279243/?locale=pt_BR"
         target="_blank"
-        aria-label="Visit us on Facebook"
+        aria-label="Nos visite no Facebook"
         className="text-gray-950 data-[hover]:text-gray-950/75"
       >
         <SocialIconFacebook className="size-4" />
       </Link>
       <Link
-        href="https://x.com"
+        href="https://wa.me/+5571996442491"
         target="_blank"
-        aria-label="Visit us on X"
+        aria-label="Converse com a gente!"
         className="text-gray-950 data-[hover]:text-gray-950/75"
       >
-        <SocialIconX className="size-4" />
+        <SocialIconWhatsApp className="size-4" />
       </Link>
       <Link
-        href="https://linkedin.com"
+        href="https://www.instagram.com/animai.festival"
         target="_blank"
-        aria-label="Visit us on LinkedIn"
+        aria-label="Nos visite no Facebook"
         className="text-gray-950 data-[hover]:text-gray-950/75"
       >
-        <SocialIconLinkedIn className="size-4" />
+        <SocialIconInstagram className="size-4" />
       </Link>
     </>
   )
@@ -172,7 +186,12 @@ export function Footer() {
             <PlusGridRow className="flex justify-between">
               <div>
                 <PlusGridItem className="py-3">
-                  <Copyright />
+                  <Copyright/>
+                </PlusGridItem>
+              </div>
+              <div className="flex">
+                <PlusGridItem className="flex items-center gap-8 py-3">
+                  <SocialLinks/>
                 </PlusGridItem>
               </div>
             </PlusGridRow>
